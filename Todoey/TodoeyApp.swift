2b458a1ff479cd7798e1b47c461ebe3c00f08509
Simran Preet Singh <simran5590@gmail.com>
2023-05-11 10:44:59 -0700

    Initial Commit

---
+++ Todoey/TodoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoeyApp.swift
+//  Todoey
+//
+//  Created by Simran Preet Narang on 2023-05-11.
+//
+
+import SwiftUI
+
+@main
+struct TodoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
